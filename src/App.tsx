c813fdc43dfaebcdc85c86b63aed3c0d765a4996
chintan-tag97 <chintan@tag97.com>
2025-01-29 12:56:50 +0530

    uploading

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Crud from './Crud.tsx';
+
+function App() {
+  return (
+    <div className="App">
+     <Crud/>
+    </div>
+  );
+}
+
+export default App;
